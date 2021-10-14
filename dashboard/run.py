d14dc76bfce3a347d7b9c6e46e0fe02460f2fd14
elpachongco <earlsiachongco@gmail.com>
2021-10-14 15:15:17 +0800

    cleanup, now opens page with browser on first startup

--- dashboard/run.py
+++ dashboard/run.py
@@ -1,23 +1,11 @@
 #! /usr/bin/env python3
-''' 
-Starts the dashboard. 
-Runs flask & Starts the browser 
-'''
-
 import webbrowser
 import os
 from subprocess import Popen
 from pathlib import Path
 
-## SET Environment variables
-#flaskProgramLocation = Path.cwd() / 'backend' / 'app.py'
-#os.environ["FLASK_APP"] = str(flaskProgramLocation)
-
-# change working directory (cwd), run flask
 runFlaskCmd = ["pipenv", "run", "flask", "run", "-p" ,"5000", "--host", "0.0.0.0"]
+# change working directory (cwd), run flask
 Popen(runFlaskCmd, cwd="./client/")
 
-# open index.html
-#indexPage = Path.cwd() / 'frontend'/ 'index.html'
-
-#webbrowser.open("http:\\\localhost:5000")
+webbrowser.open("http:\\\localhost:5000")
