5d07eb4d0145f4b7acf224f55b0207e919dd09d4
elpachongco <earlsiachongco@gmail.com>
2021-10-15 14:40:37 +0800

    modified to start both dashboard and main on start

--- startup.bat
+++ startup.bat
@@ -1,1 +1,6 @@
-py .\main.py
+:: run commands in new cmd window with "start cmd /k"
+:: use /c to auto exit when finish
+start cmd /c py .\main.py
+cd .\dashboard
+start cmd /c py .\run.py
+:: pyw - run python program in headless mode (no window spawned)
