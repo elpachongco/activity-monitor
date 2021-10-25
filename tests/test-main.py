016c6f8a11651e764208e063a25c97b0a483bc9f
elpachongco <earlsiachongco@gmail.com>
2021-10-25 12:06:30 +0800

    added tests

---
+++ tests/test-main.py
@@ -0,0 +1,17 @@
+import unittest
+
+"""
+Things to test:
+
+- Check output of tracker (activity dict), and output of uploader (sqlite entry)
+- Check if environment variable "ACTIVITY_DB" exists.
+
+"""
+
+class TestMain(): 
+
+   def testUpload(self):
+       pass
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test-uploader.py
@@ -0,0 +1,16 @@
+import unittest
+
+"""
+Things to test:
+
+- Check uploader.dbpath if it created a db
+
+"""
+
+class TestTracker(): 
+
+   def testUpload(self):
+       pass
+
+if __name__ == "__main__":
+    unittest.main()
