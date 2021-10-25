a368a3c14c3511c2ebfc3311435b36bf105f14bb
elpachongco <earlsiachongco@gmail.com>
2021-10-25 15:41:26 +0800

    removed setUp(), removed testProcess()

--- tests/test-main.py
+++ tests/test-main.py
@@ -1,4 +1,8 @@
 import unittest
+import os
+import sys
+import sqlite3
+from pathlib import Path
 
 """
 Things to test:
@@ -8,10 +12,39 @@ Things to test:
 
 """
 
-class TestMain(): 
+# Change working directory to the parent directory (only for import)
+sys.path.insert(0, os.path.abspath(".."))
+import main
 
-   def testUpload(self):
-       pass
+class TestMain(unittest.TestCase): 
+
+    def setUp(self):
+        pass
+
+    def testDbPath(self):
+        """
+        Test if main and uploader have the same path for db
+        """
+        self.dbPath = main.environ["ACTIVITY_DB"]
+        self.sqlConnection = sqlite3.connect("../activity.db")
+        self.assertEqual(str(self.dbPath), str(main.uploader.dbPath))
+
+    # def testProcess(self):
+    #     """
+    #     Tests whether the returned activity dictionary is inputted to the
+    #     sqlite database properly.
+    #     Needs to have the user change the window.
+    #     """
+
+    #     activity = main.tracker.track()
+
+    #     # Get last item from activity.db
+    #     dbTail = self.sqlConnection.execute("SELECT * FROM activity_data \
+    #     ORDER BY actStart DESC LIMIT 1;")
+
+    #     self.assertEqual(activity["windowName"], dbTail.fetchone()[3]) 
+    #     self.sqlConnection.close()
+
 
 if __name__ == "__main__":
     unittest.main()
