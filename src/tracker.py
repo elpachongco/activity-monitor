747fece6a08c76694bf2a753d9d9ea06b5c25fc9
elpachongco <earlsiachongco@gmail.com>
2023-07-30 17:03:27 +0800

    Integrate `desktopspy`.

--- src/tracker.py
+++ src/tracker.py
@@ -1,145 +1,11 @@
-import csv
-import ctypes
 import logging
 import platform
-import subprocess
 import time
 
+import psutil
+from desktopspy.trackers import getForegroundWindow, isUserActive
+
 logger = logging.getLogger()
-
-if platform.system() == "Windows":
-    # ctypes handles the Windows-specific functions
-    from ctypes import byref, create_unicode_buffer, windll, wintypes
-
-
-class LASTINPUTINFO(ctypes.Structure):
-    # Special class for storing lastinputinfo data from windows
-    _fields_ = [("cbSize", ctypes.c_ulong), ("dwTime", ctypes.c_ulong)]
-
-
-def getActivityInfo(os):
-    # Get window name and process name of current foreground window
-    # Makes use of windows API
-    # RETURNS: str name of window, str name of process
-    if os == "Windows":
-        # Get the unique window ID of the foreground window
-        windowId = windll.user32.GetForegroundWindow()
-
-        # get title length of the window id (windowId)
-        titleLength = windll.user32.GetWindowTextLengthW(windowId)
-
-        # Create a buffer to put the title on
-        # titleLength + 1 because C strings require an additional character
-        # '\0' to terminate strings, I assume
-        titleBuffer = create_unicode_buffer(titleLength + 1)
-
-        # Get window text of the given window ID (windowId), store it to the
-        # second argument (titleBuffer), and use 3rd arg as max length of text incl '\0'.
-        windll.user32.GetWindowTextW(windowId, titleBuffer, titleLength + 1)
-
-        # creates a dword type variable which will store the Process ID of the
-        # foreground window
-        pid = wintypes.DWORD()
-
-        # Get process id of the given window ID (arg 1) and store it to
-        # pid variable
-        windll.user32.GetWindowThreadProcessId(windowId, byref(pid))
-
-        # Run command 'tasklist', lookup which program has the pid
-        # and store CSV output to var
-        tasklist = os.popen(
-            f'tasklist /FI \
-						"pid eq {pid.value}" /FO CSV'
-        )
-
-        processNameCSV = tasklist.read()
-        tasklist.close()
-
-        # Returns a csv reader object
-        readCSV = csv.reader(processNameCSV)
-        listCSV = list(readCSV)
-
-        # Position of the program name in the list
-        # I wouldn't consider this portable so this might change.
-        exeName = listCSV[10][0]
-
-        # Return the window name, & process name running the window.
-        return titleBuffer.value, exeName
-
-    if os == "Linux":
-        result = subprocess.run(
-            ["timeout", "1", "xdotool", "getwindowfocus", "getwindowpid"],
-            capture_output=True,
-            text=True,
-        )
-
-        # pid is blank if no window is focused.
-        if result.stdout == "":
-            return "", ""
-
-        proc = int(result.stdout)
-
-        result = subprocess.run(
-            ["timeout", "1", "ps", "-p", str(proc), "-o", "comm="],
-            capture_output=True,
-            text=True,
-        )
-        processName = result.stdout.strip("\n")
-
-        result = subprocess.run(
-            ["timeout", "1", "xdotool", "getwindowfocus", "getwindowname"],
-            capture_output=True,
-            text=True,
-        )
-
-        windowName = result.stdout.strip("\n")
-        return windowName, processName
-
-    logger.error("OS is Unknown: %s", os)
-    raise Exception("OS is Unknown: %s", os)
-
-
-def getUserIsActive(lastInputInfo, os, minGap=800):
-    # Compares gap between last time of input from mouse or kb and current time.
-    # ARGS:
-    # 	lastInputInfo -> Instance of class LASTINPUTINFO(ctypes.Structure),
-    # 	minGap -> Int, minimum time between activity in mseconds
-    # 	os -> string, the platform the user is using
-
-    ### TODO: Fix minGap linux implementation.
-
-    # Store last input time to class
-    if os == "Windows":
-        windll.user32.GetLastInputInfo(byref(lastInputInfo))
-        lastInputTime = lastInputInfo.dwTime
-
-        currentTime = windll.kernel32.GetTickCount()
-
-        timeGap = currentTime - lastInputTime
-
-        return timeGap <= minGap
-
-    if os == "Linux":
-        # Returns: 234324.2 234234.3
-        # See proc man page for /proc/uptime
-        # result = subprocess.run(["cat", "/proc/uptime"], capture_output=True, text=True)
-        # Get first item only
-        # currentTimeMs = float(result.stdout.split(" ")[0]) * 1000
-
-        result = subprocess.run(
-            # Implementation depends on this command:
-            # timeout .1 xinput test-xi2 --root
-            # If an input is made, EVENT is present.
-            # timeout should be < mingap
-            ["timeout", str((minGap / 1000) / 2), "xinput", "test-xi2", "--root"],
-            capture_output=True,
-            text=True,
-        )
-        # print( "EVENT" in result.stdout)
-        return "EVENT" in result.stdout
-
-    logger.error("OS is Unknown: %s")
-    raise Exception("OS is Unknown: %s", os)
 
 
 class Tracker:
@@ -158,10 +24,6 @@ class Tracker:
     def __init__(self):
         self.os = platform.system()
         logger.info("OS detected: %s", self.os)
-
-        if self.os == "Windows":
-            self.lastInputInfo = LASTINPUTINFO()
-            self.lastInputInfo.cbSize = ctypes.sizeof(LASTINPUTINFO)
 
     def track(self):
         """
@@ -201,8 +63,9 @@ class Tracker:
             time.sleep(self.WHILEINTERVAL)
 
             # Detects user Inactivity
-            userIsActive = getUserIsActive(self.lastInputInfo, self.os)
-            currentWindow, currentProcess = getActivityInfo(self.os)
+            userIsActive = isUserActive(self.os)
+            currentWindow, currentPid = getForegroundWindow(self.os)
+            currentProcess = psutil.Process(currentPid).name()
 
             # When track() is ran for the first time, windowName, procesName is ""
             # Set current window as windowname, and proceed to next loop cycle.
